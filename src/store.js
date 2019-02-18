9a18d16d8ab97be7a9fa2ec549a53326b3f11cc3
Patrick Narciso <patrick.ncsilva@gmail.com>
2019-02-18 00:48:10 -0300

    feat: create the base store

--- src/store.js
+++ src/store.js
@@ -5,12 +5,47 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    people: [],
+    movies: [],
+    vehicles: [],
+    char: {
+      name: '',
+      specie: '',
+      height: '',
+      hair: '',
+      planet: ''
+    },
   },
   mutations: {
-
+    SET_PEOPLE(state, payload) {
+      state.people = payload;
+    },
+    SET_MOVIES(state, payload) {
+      state.movies.push(payload);
+    },
+    SET_VEHICLES(state, payload) {
+      state.vehicles = payload;
+    },
+    SET_CHAR(state, payload) {
+      state.char.specie = payload.specie;
+      state.char.height = payload.height;
+      state.char.hair = payload.hair;
+      state.char.planet = payload.planet;
+      state.char.name = payload.name;
+    }
   },
   actions: {
-
+    SET_PEOPLE(context, payload) {
+      context.commit('SET_PEOPLE', payload);
+    },
+    SET_MOVIES(context, payload) {
+      context.commit('SET_MOVIES', payload);
+    },
+    SET_VEHICLES(context, payload) {
+      context.commit('SET_VEHICLES', payload);
+    },
+    SET_CHAR(context, payload) {
+      context.commit('SET_CHAR', payload);
+    }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <Container alignH="center" alignV="center">
     <Row>
-      <Card>
+      <Card width="70vw">
         <Photo 
           src="https://s3-sa-east-1.amazonaws.com/star-quiz/vader.png" 
           alt="Ícone do Darth Vader"
-          maxWidth="100">
+          width="100px">
         </Photo>
         <CardTitle>starquiz!</CardTitle>
         <CardBody>
           <CardText size="12">Bem-Vindo grande Padawan!</CardText> 
           <CardText size="12">Com esse quiz você terá oportunidade de identificar os principais personagens de Star- wars, marcar pontos e se tornar um expert nesta série de filmes maravilhosa!</CardText>
         </CardBody>
-        <BtnAction>
+        <BtnAction v-on:click="play" primary>
           <CardText size="16" animate>Jogar</CardText>
         </BtnAction>
       </Card>
@@ -41,9 +41,13 @@ export default {
     Photo,
     CardTitle,
     CardText,
-    Text,
     CardBody,
     BtnAction
+  },
+  methods: {
+    play () {
+      this.$router.push('game/1');
+    }
   }
 }
 </script>
